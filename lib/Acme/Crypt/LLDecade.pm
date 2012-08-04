d57fc1cc3a6a1a072aed08870cefc0265d4cbc2a
xaicron <xaicron@gmail.com>
2012-08-04 16:34:57 +0900

    initial commit

---
+++ lib/Acme/Crypt/LLDecade.pm
@@ -0,0 +1,42 @@
+package Acme::Crypt::LLDecade;
+
+use strict;
+use warnings;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Acme::Crypt::LLDecade -
+
+=head1 SYNOPSIS
+
+  use Acme::Crypt::LLDecade;
+
+=head1 DESCRIPTION
+
+Acme::Crypt::LLDecade is
+
+=head1 AUTHOR
+
+xaicron E<lt>xaicron@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2012 - xaicron
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
